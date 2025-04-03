ecee208298ce4c2982c86a0c7cc515dbcd7e387e
Max Krummenacher <maxkrummenacher@bluewin.ch>
2025-04-03 14:54:34 +0200

    added retry and refactor

--- api/__main__.py
+++ api/__main__.py
@@ -43,6 +43,49 @@ def id_to_person(id: int) -> str:
 LOGS_DIR = "./logs/"
 
 
+def make_log_from_starter(starter) -> None:
+    try:
+        name = id_to_person(starter["id"])
+    except KeyError:
+        eprint(f"skipped unnamed starter")
+        return
+
+    current = starter.get("current")
+    if current == None:
+        eprint(f"skipped {name}, because there was no 'current' object")
+        return
+
+    try:
+        time = dt.datetime.fromisoformat(current["time"].replace("Z", "+01:00"))
+    except:
+        eprint(f"failed to get time for {name}")
+        time = dt.datetime.now()
+
+    speed = current.get("speedRaw")
+
+    try:
+        lat = current["lat"]
+        lng = current["lng"]
+    except KeyError:
+        eprint(f"skipped {name} because it did not contain position")
+        return
+
+    # this can be null in json
+    device = starter.get("device")
+    if device == None:
+        battery = None
+        online = None
+    else:
+        battery = device.get("battery")
+        online = device.get("online")
+
+    # also creates a file if it doesn't exist
+    with open(f"logs/{name}.txt", mode="a") as file:
+        file.write(
+            f"{time.isoformat()}, {speed}, ({lat}, {lng}), {battery}, {online}\n"
+        )
+
+
 def log_cycle():
     response = requests.get(API_URL)
     if response.status_code != 200:
@@ -56,54 +99,20 @@ def log_cycle():
         return
 
     for starter in obj["starters"]:
-        try:
-            name = id_to_person(starter["id"])
-        except KeyError:
-            eprint(f"skipped unnamed starter")
-            continue
-
-        current = starter.get("current")
-        if current == None:
-            eprint(f"skipped {name}, because there was no 'current' object")
-            continue
-
-        try:
-            time = dt.datetime.fromisoformat(current["time"].replace("Z", "+01:00"))
-        except:
-            eprint(f"failed to get time for {name}")
-            time = dt.datetime.now()
-
-        speed = current.get("speedRaw")
-
-        try:
-            lat = current["lat"]
-            lng = current["lng"]
-        except KeyError:
-            eprint(f"skipped {name} because it did not contain position")
-            continue
-
-        # this can be null in json
-        device = starter.get("device")
-        if device == None:
-            battery = None
-            online = None
-        else:
-            battery = device.get("battery")
-            online = device.get("online")
-
-        # also creates a file if it doesn't exist
-        with open(f"logs/{name}.txt", mode="a") as file:
-            file.write(
-                f"{time.isoformat()}, {speed}, ({lat}, {lng}), {battery}, {online}\n"
-            )
+        make_log_from_starter(starter)
 
 
 def main():
     while True:
-        try:
-            log_cycle()
-        except:
-            eprint("log cycle failed")
+        for i in range(10):
+            try:
+                log_cycle()
+                break
+            except:
+                eprint(f"this log cycle failed {i+1} times")
+                time.sleep(5)
+        else:
+            eprint("gave up for this log cycle")
         time.sleep(60 * 1.5)
 
 
